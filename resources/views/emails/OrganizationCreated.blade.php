a970912d8a850d6af0b80cbb2bb049405d177224
developerkamlesh <kamlesh@recruitcrm.io>
2023-04-18 01:05:55 +0530

    Organization register email done

---
+++ resources/views/emails/OrganizationCreated.blade.php
@@ -0,0 +1,9 @@
+<h1>Yeah Hii {{ $user->name }}!!!!!!!</h1>
+<h3>Welcome to Tasks Management Application</h3>
+<p>Congratulations you have successfully register your organization to our portal enjoy our feature and manage your work force.</p><br>
+<h3>Thank Your So Much for Registrations.</h3>
+<h4>Here Details of Your Oragnization</h4>
+<p>Organization Name : {{ $organization->org_name }}</p>
+<p>Organization Email : {{ $organization->org_email }}</p>
+<p>Your Name as Director : {{ $user->name }}</p>
+<p>Your Email : {{ $user->email }}</p>
