946b40eaa17e411b73aa54903077135ff4d9e3e1
Navrachit <129142312+Navrachit@users.noreply.github.com>
2023-04-18 17:33:52 +0530

    Updated Backend

---
+++ app/Http/Controllers/ManagerController.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ManagerController extends Controller
+{
+    public function projects(Request $request){
+   
+       $managerId= $request->query('manager');
+
+      $projects=Project::all()->where('manager_id', $managerId);
+       return $projects;
+    }
+
+    public function workers(Request $request){
+        $managerId= $request->query('manager'); //3
+
+        $organisationId = DB::table('users')->where('id', $managerId)->value('organization_id'); //2
+    
+        $workers = User::all()->where('role_id', '4')->where('organization_id', $organisationId); //
+        return $workers;
+    }
+
+    public function single_project(Request $request){
+        $project_id = $request->query('id');
+
+        $project = DB::table('projects')->where('id', $project_id)->get();
+        return $project;    
+    }
+
+
+    public function update_project_tasks(Request $request){
+        //return $request->id;
+
+        $project = Project::find($request->id);
+        $total_tasks = Task::all()->where('project_id', $request->id)->count();
+        $completed_tasks = Task::all()->where('project_id', $request->id)->where('status_id', 4)->count();
+      
+        $project['total_tasks']= $total_tasks;
+        $project['tasks_completed']= $completed_tasks;
+
+        $result = $project->save();
+        
+
+        if($result){
+            return ["status"=> "Your tasks have been updated"];
+        }
+        else{
+            return ["status"=> "operation failed"];
+        }
+    }
+
+    public function single_worker(Request $request){
+        $worker_id = $request->query('id');
+
+        $worker = DB::table('users')->where('id', $worker_id)->get();
+        return $worker;    
+    }
+
+    public function get_assigned_tasks(Request $request){
+       $id= $request->id;
+        $assigned_tasks=DB::table('tasks')->where('worker_id', $id)->get();
+        return $assigned_tasks;
+
+    }
+
+    public function tasks(Request $request){
+        $project_id = $request->query('id');
+        
+        $tasks = DB::table('tasks')->where('project_id', $project_id)->get();
+      
+       foreach ($tasks as $task) {
+            $task->worker_id = User::where('id',$task->worker_id)->value('name');
+        }
+
+        return $tasks;        
+    }
+
+    public function add_task(Request $request){
+
+      /* $request->validate([
+        'title'=>'required|min:5|max:30',
+        'description'=>'required|min:5|max:30',
+        'assigned_at'=>'required',
+        'estimated_deadline'=>'required',
+        'worker_id'=>'required',
+       ]);*/
+        $task = new Task;
+       
+        $task['title'] = $request->title;
+        $task['description'] = $request->description;
+        $task['worker_id'] = $request->workerId;
+        $task['assigned_at'] = $request->assignedDate;
+        $task['estimated_deadline'] = $request->estimatedDeadline;
+        $task['review_passed'] = 0;
+        $task['status_id'] = 1;
+        $task['project_id'] =$request->projectId;
+        $result = $task->save();
+
+        $project = Project::find($request->projectId);
+        $project['total_tasks'] = $project['total_tasks']+1;
+
+        $result2=$project->save();
+        if($result && $result2){
+            return ["status"=> "Your data has been saved"];
+        }
+        else{
+            return ["status"=> "operation failed"];
+        }
+
+
+    }
+
+        public function edit_task(Request $request){
+            /*$request->validate([
+                'title'=>'required|min:5|max:30',
+                'description'=>'required|min:5|max:30',
+                'assigned_at'=>'required',
+                'estimated_deadline'=>'required',
+                'worker_id'=>'required',
+               ]);*/
+            //return $request->id;
+            $task = Task::find($request->id);
+            $task['title'] = $request->title;
+            $task['description'] = $request->description;
+            $task['worker_id'] = $request->workerId;
+            $task['assigned_at'] = $request->assignedDate;
+            $task['estimated_deadline'] = $request->estimatedDeadline;
+            $task['review_passed'] = 0;
+
+            $result = $task->save();
+        if($result){
+            return ["status"=> "Your data has been updated"];
+        }
+        else{
+            return ["status"=> "operation failed"];
+        }
+
+
+    }
+    public function delete_task($id){
+        $comments = Comment::where('task_id', $id)->delete();
+        $task=Task::find($id);
+        $result =$task->delete();
+
+        
+
+
+        if($result){
+            return ["status"=> "The task has been deleted"];
+        }
+        else{
+            return ["status"=> "operation failed"];
+        }
+    }
+
+   
+
+    public function review_task(Request $request){
+        //return $request->id;
+        $manager_id= $request->id;
+        $assigned_projects=Project::all()->where('manager_id', $manager_id); 
+        $assigned_tasks=[];
+        foreach($assigned_projects as $assigned_project){
+            $assigned_temp_tasks=Task::all()->where('project_id', $assigned_project->id)->where('status_id',3);
+            //$assigned_tasks=$assigned_temp_tasks
+            $assigned_tasks[]=array($assigned_temp_tasks);
+        }
+        //$assigned_tasks=DB::table('tasks')->where('worker_id', $id)->get();
+        return $assigned_tasks;
+    }
+
+    public function approve_task(Request $request){
+        $project=Project::find($request->projId);
+        $task = Task::find($request->id);
+
+        $task['status_id']=4;
+        $task['review_passed']=1;
+
+        $result = $task->save();
+
+        $total_tasks = Task::all()->where('project_id', $request->projId)->count();
+        $completed_tasks = Task::all()->where('project_id', $request->projId)->where('status_id', 4)->count();
+
+        if($total_tasks === $completed_tasks){
+            $project['completed_at'] = date('Y-m-d H-i-s');
+            $project->save();
+        }
+     
+
+        if($result){
+            return ["status"=>"task approved successfully"];
+        }
+        else{
+            return ["status"=>"operation failed"];
+        }
+
+
+    } 
+    public function reject_task(Request $request){
+        $task = Task::find($request->id);
+
+        $task['status_id']=1;
+
+        $result = $task->save();
+
+        if($result){
+            return ["status"=>"task rejected successfully"];
+        }
+        else{
+            return ["status"=>"operation failed"];
+        }
+    }
+    
+    public function worker_names(Request $request){
+        //return $request->id;
+        $organisation_id = User::where('id',$request->id)->value('organization_id');
+        //return $organisation_id;
+        
+        $worker_names = User::select('id','name')->where('organization_id',$organisation_id)->where('role_id', 4)->get();
+        //$worker_names = DB::table('users')->select('name')->where('id', $request->id)->where('role_id', 4)->get();
+        return $worker_names;
+    }
+
+    public function toggle_visibility(Request $request){
+        //return $request->id;
+        $project=Project::find($request->id);
+        
+        if($project['workers_visibility']==1){
+            $project['workers_visibility']=0;
+        }
+        else{
+            $project['workers_visibility']=1;
+        }
+
+        $result=$project->save();
+
+        if($result){
+            return $project['workers_visibility'];
+            
+        }else{
+            return ["status"=>"operation failed"];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -86,3 +86,24 @@ Route::get('task/{task_id}/comments',[CommentController::class, 'task_comments']
 Route::post('/comments',[CommentController::class, 'store']);
 Route::put('comments/{comment_id}',[CommentController::class, 'update']);
 Route::delete('/comments/{comment_id}',[CommentController::class, 'destroy']);
+
+
+//Manager Controller
+Route::get('project', [ManagerController::class, 'projects']);
+Route::get('worker', [ManagerController::class, 'workers']);
+Route::get('single_worker', [ManagerController::class, 'single_worker']);
+Route::get('single_project', [ManagerController::class, 'single_project']);
+//Route::get('single_task', [ManagerController::class, 'single_task']);
+Route::put('toggle_visibility', [ManagerController::class, 'toggle_visibility']);
+Route::put('update_project_tasks', [ManagerController::class, 'update_project_tasks']);
+Route::get('tasks', [ManagerController::class, 'tasks']);
+
+Route::post('add_task', [ManagerController::class, 'add_task'] );
+Route::put('edit_task', [ManagerController::class, 'edit_task'] );
+Route::get('worker_names', [ManagerController::class, 'worker_names']);
+Route::delete('delete_task/{id}', [ManagerController::class, 'delete_task'] );
+
+Route::get('assigned_tasks', [ManagerController::class, 'get_assigned_tasks']);
+Route::get('review_tasks', [ManagerController::class, 'review_task']);
+Route::put('approve_task', [ManagerController::class, 'approve_task']);
+Route::put('reject_task', [ManagerController::class, 'reject_task']);
