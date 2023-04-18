500a7add179c38a39bcdb6636ebd01285e85298f
developerkamlesh <kamlesh@recruitcrm.io>
2023-04-18 15:36:10 +0530

    login change

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -59,5 +59,12 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
+
+        RateLimiter::for('login', function (Request $request) {
+            return [
+                Limit::perMinute(500),
+                Limit::perMinute(4)->by($request->input('email')),
+            ];
+        });
     }
 }
